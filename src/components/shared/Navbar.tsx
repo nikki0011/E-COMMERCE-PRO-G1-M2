d9bcf5ebdf8bb0445d28c4f149d39ccd5affcd60
Ignacio Holmquist <ignacioholmquist12@gmail.com>
2026-05-14 10:32:06 -0300

    Cambiamos estilos en Main

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     }`;
 
   return (
-    <nav className="bg-neutral-600 border-b text-zinc-100 text-lg py-3 sticky top-0 z-50">
+    <nav className="bg-neutral-600 text-zinc-100 text-lg py-3 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
