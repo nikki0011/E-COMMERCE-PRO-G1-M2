ca43746d15c48fe89636a111c9c3ad1bf3253ed7
Ignacio Holmquist <ignacioholmquist12@gmail.com>
2026-05-14 09:40:34 -0300

    Estructuramos y bajamos el Footer para luego agregarlo a App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from "react-router";
 import Navbar from "./components/shared/Navbar";
-
+import Footer from "./components/shared/Footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
+        <main className="grow min-h-screen"></main>
+        <Footer />
         </BrowserRouter>
     </>
   );
--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,10 +1,11 @@
-
 const Footer = () => {
-    return (
-        <div>
-          <p>footer</p>  
-        </div>
-    );
+  return (
+    <footer className="bg-neutral-800 text-white py-4 text-center">
+        <h3 className="text-lg font-bold uppercase">Hard Stocks</h3>
+        <p>&copy; {new Date().getFullYear()} Todos los derechos reservados.</p>
+        <p className="text-sm mt-2 text-stone-300">Develop by <span className="font-bold">Funes Gabriel - Paez Nair - Holmquist Ignacio</span></p>
+    </footer>
+  );
 };
 
 export default Footer;
