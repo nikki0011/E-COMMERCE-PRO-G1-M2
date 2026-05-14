413c80bcf62f88825014e5c94e849f832afabc9e
Ignacio Holmquist <ignacioholmquist12@gmail.com>
2026-05-14 09:59:36 -0300

    Agregamos el icono al footer y cambiamos background del Navbar y tmb el 'Active' y 'Hover' de los botones del mismo

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,9 +1,19 @@
+import { AiOutlineDoubleLeft } from "react-icons/ai";
+
 const Footer = () => {
   return (
     <footer className="bg-neutral-800 text-white py-4 text-center">
+      <div className="flex items-center justify-center gap-2 mb-2">
+        <AiOutlineDoubleLeft className="text-slate-100 text-2xl" />
         <h3 className="text-lg font-bold uppercase">Hard Stocks</h3>
-        <p>&copy; {new Date().getFullYear()} Todos los derechos reservados.</p>
-        <p className="text-sm mt-2 text-stone-300">Develop by <span className="font-bold">Funes Gabriel - Paez Nair - Holmquist Ignacio</span></p>
+      </div>
+      <p>&copy; {new Date().getFullYear()} Todos los derechos reservados.</p>
+      <p className="text-sm mt-2 text-stone-300">
+        Develop by{" "}
+        <span className="font-bold">
+          Funes Gabriel - Paez Nair - Holmquist Ignacio
+        </span>
+      </p>
     </footer>
   );
 };
