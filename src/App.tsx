fd7d92d91890e5936035a2c6459c41715fa89b49
Ignacio <ignacioholmquist12@gmail.com>
2026-05-15 17:58:33 -0300

    Agg el inicio por error de Pull Request

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Navbar from "./components/shared/Navbar";
 import Footer from "./components/shared/Footer";
+import Inicio from "./components/pages/Inicio";
 import Login from "./components/pages/Login";
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
         <Navbar />
         <main className="grow flex justify-center bg-linear-to-br from-[#d6d3d1] to-[#a8a6a4]">
           <Routes>
+            <Route path="/" element={<Inicio></Inicio>} />
             <Route path="/login" element={<Login></Login>} />
           </Routes>
         </main>
