06f6363af1585ac4341f2e33dc512ab3ca39e28e
Ignacio <ignacioholmquist12@gmail.com>
2026-05-15 11:24:16 -0300

    Estructuramos el maquetado del login

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import Navbar from "./components/shared/Navbar";
 import Footer from "./components/shared/Footer";
+import Login from "./components/pages/Login";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
-        <main className="grow bg-linear-to-br from-[#d6d3d1] to-[#a8a6a4]"></main>
+        <main className="grow flex justify-center bg-linear-to-br from-[#d6d3d1] to-[#a8a6a4]">
+          <Routes>
+            <Route path="/login" element={<Login></Login>} />
+          </Routes>
+        </main>
         <Footer />
         </BrowserRouter>
     </>
