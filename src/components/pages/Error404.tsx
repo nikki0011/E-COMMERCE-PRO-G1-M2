3135874c8861e9fda5b6de37b471061b7e111d4b
Nair Paez <nairpaez@outlook.com>
2026-05-16 11:23:24 -0300

    agregamos imagen de forma local en carpeta assets

--- src/components/pages/Error404.tsx
+++ src/components/pages/Error404.tsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router';
+import imgError from "../../assets/error-404.png"
 
 const Error404 = () => {
     return (
         <div className='flex flex-col justify-center items-center gap-7 my-7'>
-            <img src={"https://cdn-icons-png.flaticon.com/512/755/755014.png"} alt="Error 404" className='w-40 md:w-60 lg:w-90' />
+            <img src={imgError} alt="Error 404" className='w-40 md:w-60 lg:w-90' />
             <p className='text-3xl font-bold md:text-4xl'>Oops!!</p>
             <p className="text-4xl">pagina no encontrada 404</p>
             <Link to={'/'} className=' text-2xl text-stone-100 bg-neutral-600 p-3 rounded-tr-4xl rounded-bl-4xl hover:underline'>Volver a inicio</Link>
