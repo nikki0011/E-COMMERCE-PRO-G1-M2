82c929dee91c53fe93ab334d55ff15b1a619c6a6
Gabriel Funes <gfunes@gmail.com>
2026-05-18 12:23:39 -0300

    se agrego la ruta en app y se agrego ruta de la imagen del producto

--- src/components/pages/DetalleProducto.tsx
+++ src/components/pages/DetalleProducto.tsx
@@ -1,5 +1,5 @@
 import { IoHeartOutline } from "react-icons/io5";
-import { useParams, useNavigate, Link } from "react-router";
+import { useParams, useNavigate,  } from "react-router";
 import { useAppContext } from "../../context/AppContext";
 import { useEffect } from "react";
 
@@ -14,7 +14,7 @@ const DetalleProducto = () => {
     useEffect(() => {
         if (!producto) {
             // Si no existe el servicio, redirigir a 404
-            navigate("/404");
+            navigate("/DetalleProducto");
         }
     }, [producto, navigate]);
 
@@ -28,16 +28,16 @@ const DetalleProducto = () => {
             
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
                 <div className="shadow-md p-4 rounded w-150">
-                    <img src="" alt="" />
+                    <img src={producto.imagen} alt={producto.nombreProducto} />
                 </div>
                 <div>
-                    <h1 className="text-3xl font-bold md-4">{producto.nombre}</h1>
-                    <p className="text-gray-600 mb-6">{producto.Descripcion}</p>
+                    <h1 className="text-3xl font-bold md-4">{producto.nombreProducto}</h1>
+                    <p className="text-gray-600 mb-6">{producto.descripcion}</p>
                     <div className="mb-6">
-                    <span className="text-3xl font-bold">{producto.Precio}</span>
+                    <span className="text-3xl font-bold">{producto.precio}</span>
                     </div>
                     <div className="mb-6">
-                        <h3 className="font-semibold mb-2">{producto.Categoria}</h3>
+                        <h3 className="font-semibold mb-2">{producto.categoria}</h3>
                     <span className="font-semibold mb-2">Categoria</span>
                     </div>
                     <button className="rounded-lg bg-cyan-700 px-8 py-3  text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800
