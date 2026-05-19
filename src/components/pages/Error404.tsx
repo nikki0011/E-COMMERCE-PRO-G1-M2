518996d31f12ccb421773533e70650fb80a4bc8b
Nair Paez <nairpaez@outlook.com>
2026-05-19 10:24:15 -0300

    corregimos alineacion de componente error-404

--- src/components/pages/Error404.tsx
+++ src/components/pages/Error404.tsx
@@ -3,7 +3,7 @@ import imgError from "../../assets/error-404.png";
 
 const Error404 = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-7 my-7">
+    <div className=" container flex flex-col justify-center items-center gap-7 my-7">
       <img src={imgError} alt="Error 404" className="w-40 md:w-60 lg:w-90" />
       <p className="text-3xl font-bold md:text-4xl">Oops!!</p>
       <p className="text-4xl">pagina no encontrada 404</p>
